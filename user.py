bbf18cfac20ac8cc9067bb3e4ffb90bd31b7d272
seron-ux <abyseronkirui@gmail.com>
2021-02-13 12:16:12 +0300

    create a method that helps define objects

--- user.py
+++ user.py
@@ -1,11 +1,17 @@
- class User:
+class User:
     """
     Class that generates new instances of user
     """
 
-     User_list [] # Empty user list
+    User_list [] # Empty user list
 
-     def __init__(self,username, password):
+    def __init__(self,username, password): #method that helps us define properties for our objects.
+    
+        self.username = username
+        self.password = password
 
-         self.username = username
-         self.password = password
+    def save_user(self):
+        '''
+        save new user to the application
+        '''
+        User.user_list.append(self)
