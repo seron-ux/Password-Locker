11d92c17229da753e7a3e2196f354bdf43079c8c
seron-ux <abyseronkirui@gmail.com>
2021-02-13 12:04:07 +0300

    create a method that helps define objects

--- user.py
+++ user.py
@@ -1,4 +1,11 @@
- class Contact:
+ class User:
     """
-    Class that generates new instances of contacts
+    Class that generates new instances of user
     """
+
+     User_list [] # Empty user list
+
+     def __init__(self,username, password):
+
+         self.username = username
+         self.password = password
