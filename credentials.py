3b39b0d9f935f53b9e7fcd8033d7c0418e3f7e38
seron-ux <abyseronkirui@gmail.com>
2021-02-14 12:22:31 +0300

    form a method that takes in account_name and returns a credentials that matches account_name

--- credentials.py
+++ credentials.py
@@ -2,11 +2,11 @@ import random
 import string
 import pyperclip
 class Credential:
-     """
-    Class that generates new instances of credentials.
-    """
+        """
+        Class that generates new instances of credentials.
+        """
 
-    Credential_list = [] # empty credentials list
+        Credential_list = [] # empty credentials list
 
 
     def __init__(self,account,username, password):
@@ -17,14 +17,28 @@ class Credential:
         self.username = username
         self.password = password
 
-    def save_credentials(self)
+     def save_credential(self):
         '''
         method that saves credential objects into application
         '''
+        Credential.credential_list.append(self)
 
-    def delete_credential(self)
+    def delete_credential(self):
         '''
         method deletes a saved credential from credential_list
         '''
 
         Credential.Credential_list.remove(self)
+
+
+    @classmethod
+    def find_credentials(cls,account):
+        """
+        method that takes in account_name and returns a credentials that matches account_name
+        """
+        for Credential in cls.credential_list:
+            if Credential.account == account:
+                return Credential
+
+                
+
